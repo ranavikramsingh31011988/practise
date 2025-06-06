fa87fd829a1ca528ca025b1766dc853011e925d6
VTS <kumarir@sts.holdings>
2025-06-06 10:28:27 +0530

    sparkccode update

--- main.py
+++ main.py
@@ -1,16 +1,5 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+from pyspark.sql import SparkSession
+spark = SparkSession.builder.appName("creatingfromcsv").getOrCreate()
+df = spark.read.csv("C:\\Users\\dell\\PycharmProjects\\practise\\text.csv", header=True,inferSchema=True)
+df.show()
+df.printSchema()
